6df8101c6c87d3015beffce26db954a00d24a875
Dotsquares <30402051+dotsquares-support@users.noreply.github.com>
2021-12-31 18:16:44 +0530

    facebook-feed

---
+++ src/translations/en/dsfacebook.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Facebook Feed plugin for Craft CMS 3.x
+ *
+ * Get Posts from Your Facebook Page
+ *
+ * @link      https://dotsquares.com
+ * @copyright Copyright (c) 2021 dotsquares
+ */
+
+/**
+ * Facebook Feed en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('facebook-feed', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    dotsquares
+ * @package   FacebookFeed
+ * @since     1.0.0
+ */
+
+
+return [ 'Facebook Access Token' => 'Facebook Access Token',
+
+    'Enter your Facebook Access Token' => 'Enter your Facebook Access Token',
+
+    'Enter your Facebook Access Token here.' => 'Enter your Facebook Access Token here.',
+
+    'Facebook Page ID' => 'Facebook Page ID',
+
+    'Enter Facebook Page ID here' => 'Enter Facebook Page ID here',
+
+    'Enter your Facebook Page ID here.' => 'Enter your Facebook Page ID here.',
+
+    'Facebook Feed Title' => 'Facebook Feed Title',
+
+    'Facebook Feed' => 'Facebook Feed',
+
+    'Switch Facebook Feed Layout From Grid Layout to Slider Layout.' => 'Switch Facebook Feed Layout From Grid Layout to Slider Layout.',
+
+    'Enable Slider Layout' => 'Enable Slider Layout',
+
+    'Disable Fields from layout which you dont want to appear on frontend feed.' => 'Disable Fields from layout which you dont want to appear on frontend feed.',
+
+    'Facebook Page Name' => 'Facebook Page Name',
+
+    'Facebook Page Profile Picture' => 'Facebook Page Profile Picture',
+
+    'Posted Time' => 'Posted Time',
+
+    'Posted Picture Caption' => 'Posted Picture Caption',
+
+
+];
